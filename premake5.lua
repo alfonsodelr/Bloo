e587fd4a440a53284049048f130122a6679ebf7e
alfonsodelr <delrosarioluis22@gmail.com>
2022-03-08 12:05:03 -0800

    Premake Setup

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Bloo"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Bloo"
+    location "Bloo"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest" 
+
+        defines
+        {
+            "BL_PLATFORM_WINDOWS",
+            "BL_BUILD_DLL"
+        }
+        
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+    
+        filter "configurations:Debug"
+            defines "BL_DEBUG"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines "BL_RELEASE"
+            optimize "On"
+
+        filter "configurations:Dist"
+            defines "BL_DIST"
+            optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "Bloo/vendor/spdlog/include",
+        "Bloo/src"
+    }
+
+    links
+    {
+        "Bloo"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "BL_PLATFORM_WINDOWS"
+        }
+    
+        filter "configurations:Debug"
+            defines "BL_DEBUG"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines "BL_RELEASE"
+            optimize "On"
+
+        filter "configurations:Dist"
+            defines "BL_DIST"
+            optimize "On"
